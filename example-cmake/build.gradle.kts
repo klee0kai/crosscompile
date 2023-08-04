c3ecffe736088411995be9573aa9501fb21d30b0
klee0kai <klee0kai@gmail.com>
2023-08-05 00:00:30 +0400

    example cmake

---
+++ example-cmake/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("klee0kai-crosscompile")
+    id("com.dorongold.task-tree") version "2.1.1"
+}
+
+
+
+crosscompile {
+
+
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,3 +20,4 @@ dependencyResolutionManagement {
 rootProject.name = "crosscompile"
 includeBuild("klee0kai-crosscompile")
 include("example-automake")
+include("example-cmake")
